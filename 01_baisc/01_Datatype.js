921f6549a14518502e5597e5582fd54f4cbf23db
Sachin Singh <ashoksachinf3@gmail.com>
2025-02-06 08:15:45 +0000

    Datatype

---
+++ 01_baisc/01_Datatype.js
@@ -0,0 +1,24 @@
+// Alert points 
+//alert(3+3) is not working here cauz this is node js not a browser 
+
+"use strict " // treat js code as new source 
+
+// semi  colan lagao ya na lagao code ko yaisa likho ki aram se readable ho 
+
+/* Note Data Type 
+
+1.number (num)
+2.string 
+3.date 
+4.null ( assigned value h ye )
+5. Undefined ( value Abhi assigned nahi h )
+6.largeint
+7. Symbols ( for Uniqueness use karte hai )
+
+Type 2 
+2.1 Object 
+
+Null ek object typed data type hai 
+
+*/
+

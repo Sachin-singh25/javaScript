9b185cf7bc6197fd2b106e2d3d6280df471d0cee
Sachin Singh <ashoksachinf3@gmail.com>
2025-02-06 09:08:22 +0000

    Conversion_D2

--- 01_baisc/01_DataType_Conversion.js
+++ 01_baisc/01_DataType_Conversion.js
@@ -1,11 +1,11 @@
 let score = true
 
 let Score=Number(score)  // idhar humne explicitly score ko string se number mai convert kra hai 
-
+/*
 console.log(typeof score); //method 1
 console.log(typeof (Score)) //method 2
 console.log(Score)   
-
+*/
 
 /* N0te
 
@@ -19,4 +19,26 @@ N5.  undefined => NaN
 let isLogged_IN = 23    // zero ko chhor k koi bhi aur value loge hamesha true hi ayega
 
 let InBoolean_isLogged_IN = Boolean( isLogged_IN)
-console.log(InBoolean_isLogged_IN);
+//console.log(InBoolean_isLogged_IN);
+
+
+/* ************* 0peration **********
+  
+Add , Subtract , Multiply , Substraction , and any other work that has been in the program is called operation ..
+
+*/
+
+
+let str1 ="Hello Sachin "
+let str2 = "Are You There "
+
+//console.log(str1+str2);
+
+
+let Value = 34
+let negValue = -Value 
+//console.log(negValue);
+
+// Additon of string and the number 
+
+console.log("1"+2)  //agar First String mai hai to uske baad sadko string ki trh treat kiya jayega aur agar last mai string hai to pahle number pe operation ho jayega aur last mai string pe kaam ho jayega 

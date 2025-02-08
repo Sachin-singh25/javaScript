5f58216e1573557040815f5b1cd1fa4a5f3e4cee
Sachin Singh <ashoksachinf3@gmail.com>
2025-02-08 12:16:15 +0000

    data base sumry

---
+++ 01_baisc/01_Comparison.js
@@ -0,0 +1,37 @@
+ // console.log("32">9);
+/*
+ console.log(undefined == 0);
+ console.log(undefined > 0);
+ console.log(undefined < 0); */
+
+
+ // === isko agar use karoge to  vo datatype bhi check karega , strict equal to kaha jata hai isko 
+
+
+ /* Data Type Breif Sumary video 
+   Data Type : 1. Premitive 2. Non Premitive ( refrence type )
+   1.a String               2.a arrys 
+   1.b number               2.b objects
+   1.c Boolen               2.c Functions
+   1.d null(empty)
+   1.e undefined 
+   1.f Symbol
+   1.g Big int 
+
+
+ */
+
+
+const heroes = [ "shaktiman","Flying Jatt","doga","Shiva","Motu Patlu ", " Neela Admi"]
+let myObject = {
+    name:"Sachin Singh Rajput",
+    age:21,
+    bdy:"25 jan 2004"
+
+}
+
+const Myfunction = function(){
+    console.log(
+        " Sachin is here "
+    );}
+console.log(typeof heroes);

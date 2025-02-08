0d5b6c9d91cda64c15bffa246ca4a8158a87fdfe
Sachin Singh <ashoksachinf3@gmail.com>
2025-02-08 17:21:19 +0000

    string seekh rha tha yrr

---
+++ 01_baisc/string.js
@@ -0,0 +1,16 @@
+//concatinate mtlb jodna ..
+let name = "Sachin Singh "
+let repoCount="1"
+let Note = " The First repo is related to JavaScript cause I like javaScript more than other langauge nd like to be a forntend developer"
+//console.log( name + repoCount+Note); // YE hai normal Zindagi
+console.log(`My name is ${name} nd There are ${repoCount} i made in gethub . ${Note}`); // this method is called strind interpolation 
+
+let gameName = new String("Sachin_Singh");
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.at(5));
+console.log(gameName.substring(0,7)); // isme negative dalne se vo zero se le lega 
+console.log(gameName.slice(-9,8)); // keval isme hi negative use kar sakte hai 
+
+
+

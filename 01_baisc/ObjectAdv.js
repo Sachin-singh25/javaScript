8cb39a31eb8bf86559c1f460ad3d3a9e2171ee24
Sachin Singh <ashoksachinf3@gmail.com>
2025-03-18 11:33:52 +0000

    json aur api dekha hai

---
+++ 01_baisc/ObjectAdv.js
@@ -0,0 +1,14 @@
+// lets move deeper in the Object 
+
+const course = {
+    title : "Java Script In Hindi ",
+    Prizing:999,
+    course_Instructor : " Hitesh Sir ",
+    Plateform:"Youtube",
+    Timeperiod:"9Months"
+
+}
+
+const {course_Instructor:Teacher}=course
+console.log(Teacher);
+//>> ise kahte hai  destructure of data in the code 

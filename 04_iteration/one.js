02d06257cce61b8f65798456fa74d5daf9d1d0b8
Sachin Singh <ashoksachinf3@gmail.com>
2025-06-09 06:40:27 +0000

    03.02

---
+++ 04_iteration/one.js
@@ -0,0 +1,23 @@
+// for 
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+// for (let index = 0; index < 5; index++) {
+//     const element = index;
+//     console.log(element);
+    
+    
+// }
+
+for (let i =1 ;i <=10; i++) {
+    console.log(`outter loop Value : ${i}`);
+    for (let j = 0; j <= 10; j++) {
+        console.log( i+"*"+ j+ "=" +i*j);
+        
+        
+    }
+    
+    
+}

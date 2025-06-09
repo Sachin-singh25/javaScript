036df70f7dd87f48999df94250ee8a315acb5a09
Sachin Singh <ashoksachinf3@gmail.com>
2025-06-09 12:30:40 +0000

    03.02

---
+++ 04_iteration/arrayloop.js
@@ -0,0 +1,25 @@
+// for of loop 
+
+const ary = [ 1,2,3,4,5]
+
+for( const num of ary){
+    //console.log(num);
+    
+
+}
+const greetings=" Hello World"
+for (const greet of greetings) {
+    //console.log(greet);
+    
+}
+
+//Maps
+
+const map=new Map()
+map.set("in", "India")
+map.set("usa"," united state of america")
+map.set("Pak", "Pakistan")
+map.set("Uk", "United Kingdom")
+
+console.log(map);
+
---
+++ 04_iteration/loop.js
@@ -0,0 +1,14 @@
+
+
+let  month = 0 ; 
+
+while (month <=5 ) {
+    console.log(`hello `);
+    month++
+}
+let score = 11;
+do {
+    console.log(`score is ${score}`);
+    score++
+    
+} while ( score<=10);

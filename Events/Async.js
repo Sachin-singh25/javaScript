c766c2a601eacdccb1c14095788439305e741a03
Sachin Singh <ashoksachinf3@gmail.com>
2025-07-02 07:17:49 +0000

    july02

---
+++ Events/Async.js
@@ -0,0 +1,10 @@
+// theroy of Asynchornous code 
+
+/* 
+   Blocking code                   Vs                Non Blocking Code 
+block the floe of program                         does not block execution of program
+read file sync                                    read file Async 
+
+
+
+*/
